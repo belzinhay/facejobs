5d6a7a85ac5817eb6657c1a6935011a139c55d67
Técnico em Informática 2021.2 <36127512021.2@senacrjedu.local>
2023-01-11 10:45:20 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'facejobs',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
